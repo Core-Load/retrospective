bde8b1e477d236bf74015354a0148a3f346c3186
hanbyeolkang <strongstar1210@gmail.com>
2025-12-10 09:28:10 +0900

    날짜 자동 치환 추가

--- scripts/issue-update.js
+++ scripts/issue-update.js
@@ -7,6 +7,11 @@ module.exports = async ({ github, context }) => {
 
     const creator = context.payload.sender.login
     const creatorName = members[creator]
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = String(today.getMonth() + 1).padStart(2, '0')
+    const day = String(today.getDate()).padStart(2, '0')
+    const yyyymmdd = `${year}-${month}-${day}`
 
     await github.rest.issues.addAssignees({
         issue_number: context.payload.issue.number,
@@ -20,7 +25,7 @@ module.exports = async ({ github, context }) => {
             issue_number: context.payload.issue.number,
             owner: context.payload.repository.owner.login,
             repo: context.payload.repository.name,
-            title: context.payload.issue.title.replace('XXX', creatorName),
+            title: context.payload.issue.title.replace('YYYY-MM-DD', yyyymmdd).replace('XXX', creatorName),
         })
     }
 }
